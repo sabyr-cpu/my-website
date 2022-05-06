b92b6241d1383f6f3ff809ccb928c03cdde8ce81
sabyr-cpu <sab.sabyr@gmail.com>
2022-05-06 21:29:29 +0600

    d4c1

--- src/components/Home/Page1.jsx
+++ src/components/Home/Page1.jsx
@@ -1,9 +1,11 @@
+import AppearingText from '../common/AppearingText'
+
 function page1() {
   return (
     <>
       <div className="header">Sabyrzhan's personal website</div>
       <div className="center">
-        <div className="greeting"></div>
+        <div className="greeting"><AppearingText text="Hello and welcome" delay={1000} time={5000} /></div>
       </div>
     </>
   )
